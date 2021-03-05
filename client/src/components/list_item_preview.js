c7f5b477ee35483bf4edb1e474c099f1e2ad25f3
joielee <2j00923@gmail.com>
2021-03-05 12:45:02 +0900

    bug fix: image rendering

--- client/src/components/list_item_preview.js
+++ client/src/components/list_item_preview.js
@@ -10,55 +10,50 @@ let blob = '';
 let base_64 = '';
 
 const ListItemPreview = ({ item }) => {
-  const [img, setImg]=useState('https://lumiere-a.akamaihd.net/v1/images/open-uri20160811-32147-15bzuw4_0f357d00.jpeg?region=0%2C0%2C600%2C600');
-  // const [file, setFile] = useState();
-  // const [resFromAxios, setResFromAxios] = useState();
-  // const [imgInfo, setImfInfo] = useState();
+  const [img, setImg] = useState('https://lumiere-a.akamaihd.net/v1/images/open-uri20160811-32147-15bzuw4_0f357d00.jpeg?region=0%2C0%2C600%2C600');
+  const [resImg, setResImg] = useState('https://www.stch.org.uk/wp-content/uploads/2020/06/question-mark.png')
+  const [resFromAxios, setResFromAxios] = useState();
+
+  const handleResult = (res) => {
+    if (res === '마동석') setResImg('http://image.kmib.co.kr/online_image/2018/1121/201811210010_13200924035737_1.jpg');
+    else if (res === '김종국') setResImg('http://www.futurekorea.co.kr/news/photo/202010/140940_144818_2331.jpg');
+    else setResImg('https://ilyo.co.kr/contents/article/images/2020/0120/1579505870009790.jpg');
+  }
 
   const onClickHandler = (e) => {
     e.preventDefault();
-
     window.open('e:', file);
-    const fileReader = new FileReader();
-    fileReader.readAsDataURL(file); // file equals blob
-    // // fileReader.readAsArrayBuffer(file);
-    fileReader.onload = function (e) {
-      // setImg(e.target.result.slice(23,)); //blob to something
-      base_64 = e.target.result.slice(23,);
-      console.log("base 64", e.target.result.slice(23,));
-    }
-
     const data = { imageFile: base_64 };
 
     setTimeout(console.log('Wait...'), 12000);
     axios.post(
-      'http://d7a7b3cfdcdb.ngrok.io/post',
+      'http://cf05802f1978.ngrok.io/post',
       data,
       {
         headers: {
-          'Content-Type': 'multipart/form-data'
-      }},
+          'Content-Type': 'multipart/form-data',
+        },
+      },
     )
       .then((result) => {
         console.log('Yes! 🙆‍♀️ ', result);
-        setResFromAxios(result.data.class_name);
-        console.log("resulr from axios: ", resFromAxios);
+        console.log('result from axios: ', resFromAxios);
+        handleResult(result.data.class_name);
       })
       .catch(error => console.log('Oppppsss 🙅‍♀️ ', error));
   };
 
   const onChange = (e) => {
     e.preventDefault();
-    // get uploaded filename
-    // setFile(e.target.files[0]); 
     file = e.target.files[0];
-    
-    // console.log(URL.createObjectURL(imageFile));
-    // setBlob(URL.createObjectURL(file)); //blob:http://localhost:5001/6871cc14-f0b4-4e75-8e81-6107a44014f4
-    blob =URL.createObjectURL(file);
-    // console.log("imagefile: ", blob);
-
-    //image rendering
+    blob = URL.createObjectURL(file);
+    const fileReader = new FileReader();
+    fileReader.readAsDataURL(file); // file equals blob
+    fileReader.onload = function (e) {
+      base_64 = e.target.result.slice(23);
+      console.log('base 64', e.target.result.slice(23));
+    };
+    // image rendering
     setImg(URL.createObjectURL(file));
   };
 
@@ -80,8 +75,9 @@ const ListItemPreview = ({ item }) => {
       </h2>
       <p>{item.description}</p>
       <input type="file" id="imageInput" multiple onChange={e => onChange(e)} name="file" accept="image/*" />
-      <img src={img} width="100" alt="uploaded" />
+      <img src={img} width="500" alt="uploaded image" />
       <button type="submit" onClick={e => onClickHandler(e)}>전송하기</button>
+      <img src={resImg} width="500" alt="result image" />
       <Link to={`view/${item.name}`}>
         <button type="button" className="btn btn-primary">Read </button>
       </Link>
