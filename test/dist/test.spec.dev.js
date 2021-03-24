e34d40787592310d8c64ecb578a738cde00065c8
joielee <2j00923@gmail.com>
2021-03-24 14:30:09 +0900

    add jest and sample test case

---
+++ test/dist/test.spec.dev.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// describe('Test', () => {
+//   test('this is sample testcase', () => {
+//     expect('1').toBe('1');
+//   })
+// });
+test('what is 1+1', function () {
+  expect(1 + 1).toEqual(2);
+});
---
+++ test/test.spec.js
@@ -0,0 +1,9 @@
+// describe('Test', () => {
+//   test('this is sample testcase', () => {
+//     expect('1').toBe('1');
+//   })
+// });
+
+test('what is 1+1', () => {
+  expect(1 + 1).toEqual(2);
+});
